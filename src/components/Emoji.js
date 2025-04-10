d398806259185d19652fbf9bc8fd30c5a07146a0
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 13:19:19 +0300

    adding the fucntion

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,15 +1,19 @@
 import EmojiPicker, {Emoji} from 'emoji-picker-react';
+import React, { useState } from 'react';
 
 const EmojiSearcher = () => {
-const [selectedEmoji, setSelectedEmoji] = useState("");
+  const [selectedEmoji, setSelectedEmoji] = useState("");
+  function onClick( emojiData) {
+    setSelectedEmoji(emojiData.unified);
+  }
 
   return (
     <div>
       <h2>Emoji Picker</h2>
       <div>
-        {selectedEmoji}
+        {selectedEmoji ? <Emoji unified={selectedEmoji} size={80} /> : null}
       </div>
-      <EmojiPicker />
+      <EmojiPicker onEmojiClick={onClick}/>
     </div>
   );
 }
