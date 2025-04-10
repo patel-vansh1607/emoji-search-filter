ab9361c16984693715aa1efde71e88028f8261e4
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-10 12:46:59 +0300

    adding fucntions

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -5,6 +5,10 @@ const [selectedEmoji, setSelectedEmoji] = useState("");
 
   return (
     <div>
+      <h2>Emoji Picker</h2>
+      <div>
+        {selectedEmoji}
+      </div>
       <EmojiPicker />
     </div>
   );
